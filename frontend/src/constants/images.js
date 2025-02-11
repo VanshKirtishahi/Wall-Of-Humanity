a019d9de53ca7905687944259df88ed5dc1c15ae
VanshKirtishahi <vanshkirtishahi@gmail.com>
2025-02-11 16:52:08 +0530

    initial commit

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,2 +1,2 @@
-const DEFAULT_DONATION_IMAGE = '/images/default-donation.jpg';
-export const DEFAULT_AVATAR = '/images/default-avatar.jpg'; 
+const DEFAULT_DONATION_IMAGE = 'https://placehold.co/600x400?text=No+Image';
+export const DEFAULT_AVATAR = 'https://placehold.co/400x400?text=No+Avatar'; 
